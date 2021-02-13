e19b11c805621cd56cb44863645b8f8cd473d7bf
ZenSorcere <zen.sorcere@gmail.com>
2021-02-12 19:18:47 -0800

    initial issue 34 code

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -18,4 +18,32 @@ router.get('/', function(req, res, next) {
   connection.end();
 });
 
+/* GET All Menu items for a specific cart - Vendor/SysAdmin */
+router.get('/Menus/:id', function(req, res, next) {
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+
+  connection.query(`SELECT Cart_Id, cartmenus.Menu_Id, Menu_Name, Menu_Description, Menu_Price, Available
+  FROM cartmenus
+  INNER JOIN menu ON cartmenus.Menu_Id = menu.Menu_Id
+  WHERE Cart_Id = ?;`, [req.params.id], (error, results) => {
+      /* if req.params.id doesn't match a Cart_Id, return 204 http status and a "no cart found msg"? */
+      if (results.length === 0) {
+        res.send('No such cart', 204);
+        /* res.status(204).send(results); */
+      } 
+      else if (error) {
+        res.sendStatus(500);
+      } else {
+
+      /*res.send(results, 200); */
+      res.status(200).send(results);
+      }
+    })
+
+  connection.end();
+});
+
+
 module.exports = router;
+
