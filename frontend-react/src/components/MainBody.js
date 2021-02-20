aff957462186905ce81608ea18b0d54e31639fa5
Biks <bikasjthapa@gmail.com>
2021-02-20 14:24:16 -0800

    Added few new js file for Issue 49

---
+++ frontend-react/src/components/MainBody.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Map from "./Map";
+
+class App extends React.Component {
+    render() {
+        return <Map/>
+    }
+}
+
+export default Map;
---
+++ frontend-react/src/components/Map.js
@@ -0,0 +1,10 @@
+function Map(props) {
+    return (
+        <div>
+            {/* <h2>CART LOCATIONS</h2> */}
+            <img src={`${window.location.origin}/MapForCartLocation.JPG`} width="500" height="350"/>
+        </div>
+    )
+}
+
+export default Map;
