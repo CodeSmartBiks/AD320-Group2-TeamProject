82e30ce8648954adedc0c9eedb0d26266e5b29c4
ZenSorcere <zen.sorcere@gmail.com>
2021-02-22 21:08:32 -0800

    css template creation

--- frontend-react/src/components/Header.js
+++ frontend-react/src/components/Header.js
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <header>
-      <img src={`${window.location.origin}/Hotdog_logo.jpg`}width="100"/>
+      <img src={`${window.location.origin}/Hotdog_logo.jpg`}/*width="100"*/ />
       <h1>HOTDOG CARTS</h1>
     </header>
       
--- frontend-react/src/components/Map.js
+++ frontend-react/src/components/Map.js
@@ -2,7 +2,7 @@ function Map(props) {
     return (
         <div>
             {/* <h2>CART LOCATIONS</h2> */}
-            <img src={`${window.location.origin}/MapForCartLocation.JPG`} width="500" height="350"/>
+            <img src={`${window.location.origin}/MapForCartLocation.JPG`} /* width="500" height="350" */ />
         </div>
     )
 }
