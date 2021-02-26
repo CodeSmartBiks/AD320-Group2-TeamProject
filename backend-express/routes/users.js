549f4cfd51daf53545f37a2b537eff9d35b57b52
Fikirte Mulugeta <fikirliya@gmail.com>
2021-02-26 13:59:26 -0800

    routes/users.js

--- backend-express/routes/users.js
+++ backend-express/routes/users.js
@@ -1,100 +1,36 @@
 var express = require('express');
 let mysql = require('mysql2');
-let dbCreds = require('../../../dbCreds.json');
+let dbCreds = require("../../../dbCreds.json");
 var router = express.Router();
 
-/* GET home page. */
-
-router.get('/', function (req, res, next) {
-
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //let mySQLQuery = "SELECT * FROM Orders";
   let connection = mysql.createConnection(dbCreds);
   connection.connect();
 
-  connection.query('SELECT * FROM Employees', (error, results, fields) => {
-    if (error) {
+  connection.query('SELECT * FROM Customer',(error, results) =>{
+    
+    if(error){
       res.send(500);
     }
     res.send(results);
-  })
-
-  connection.end();
-
-});
-
-/* GET All Menu items for a specific cart - Vendor/SysAdmin */
-router.get('/Menus/:id', function(req, res, next) {
-  let connection = mysql.createConnection(dbCreds);
-  connection.connect();
-
-  connection.query(`SELECT Cart_Id, cartmenus.Menu_Id, Menu_Name, Menu_Description, Menu_Price, Available
-  FROM cartmenus
-  INNER JOIN menu ON cartmenus.Menu_Id = menu.Menu_Id
-  WHERE Cart_Id = ?;`, [req.params.id], (error, results) => {
-      /* if req.params.id doesn't match a Cart_Id, return 204 http status and a "no cart found msg"? */
-      if (results.length === 0) {
-
-      /* I Can't get it to display the below message if it's already successfully displayed cart info
-          UPDATE--apparently 204 won't update the results displayed. 404 actually fixes the issue as desired */
-        res.status(404).send("Cart Not Found");
-        /* res.status(204).send('Cart Not Found'); */
-      } 
-      else if (error) {
-        res.sendStatus(500);
-      } else {
-
-      /*res.send(results, 200); */
-      res.status(200).send(results);
-      }
-    })
-/* test comment */
-  connection.end();
-
-});
-
-module.exports = router;
-
-//get all the cart details when customer makes a get call - Customer
-router.get('/:id', function (req, res, next) {
-
-  let getCartDetails = `SELECT Cart_Id,Menu_Id,Employee_FirstName,Cart_Location,Menu_Name,Menu_Description,Menu_Price FROM employees e JOIN carts c
-  USING(Employee_id )
-  JOIN cartmenus cm
-  USING(Cart_Id)
-  JOIN menu m
-  USING(Menu_Id)
- where Cart_Id= '${req.params.id}' AND Available = 'Y';`;
-
-  let connection = mysql.createConnection(dbCreds);
-  connection.connect();
-
-  connection.query(getCartDetails, (error, results) => {
-    if (error) {
-      res.send(500);
-    } else {
-      res.send(results);
-    }
-
-  })
+  }) 
 
   connection.end();
 });
 
-
-
-// API GET path for seeing all current order for a single cart - Vendor
-router.get('/Orders/:id', function(req, res, next) {
+ /* router.get('/Orders/:id', function(req, res, next) {
   
   let connection = mysql.createConnection(dbCreds);
   connection.connect();
 
-  connection.query(`SELECT orders.Order_Id,Order_Total,Customer_Id,Order_Date,Order_Status, ordersitems.OrderItem_Id FROM Orders 
+  connection.query(`SELECT Order_Total,Customer_Id,Order_Date,Order_Status, ordersitems.OrderItem_Id FROM Orders 
   INNER JOIN ordersDetails ON Orders.Order_Id = ordersDetails.Order_Id
   INNER JOIN ordersItems ON ordersdetails.OrderItem_Id = ordersitems.OrderItem_Id
   
   WHERE Cart_Id = ?;`,[req.params.id],  (error, results) =>{
-   /*  if (results == undefined){
-      res.status(404).send("Order unavailable");
-    } */
+  
 
     if(error){
       res.send(500);
@@ -107,8 +43,63 @@ router.get('/Orders/:id', function(req, res, next) {
 
   connection.end();
 });
+ */
+router.get('/Orders/order/:id', function(req, res, next) {
+  
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
 
+  connection.query(`SELECT Orders.Order_Id,Order_Total,Customer_Id,Order_Date,Order_Status, Cart_Id, ordersitems.OrderItem_Id, Menu_Name, Menu_Price, Quantity FROM Orders 
+  
+  INNER JOIN ordersDetails ON Orders.Order_Id = ordersDetails.Order_Id
+  INNER JOIN ordersItems ON ordersdetails.OrderItem_Id = ordersitems.OrderItem_Id
+  INNER JOIN menu ON menu.Menu_Id = ordersitems.Menu_Id  
+  
+  WHERE Orders_Id = ?;`,[req.params.id],  (error, results) =>{
 
+    if (results.length === 0) {
+      res.status(404).send("Order Not Found");
+    }
+    else if (error) {
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(results);
+    }
+  })
+  connection.end();
+});
+
+/* router.post('/', function(req, res, next) {
+  let mySQLQuery = `INSERT INTO Customer (Customer_Id, Customer_FirstName) VALUES (${req.body.Customer_Id}, '${req.body.Customer_FirstName}')`;
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+
+  connection.query(mySQLQuery,(error, results) =>{
+    if (error){
+        console.log(error)
+        res.send(500);
+    }
+    res.send(results);
+  }) 
+
+  connection.end();
+}); */
+
+/* router.put('/:id', function(req, res, next) {
+  
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+
+  let mySQLQuery = `UPDATE Customer SET name = '${req.body.name}' WHERE id = ${req.params.id}`;
+  connection.query(mySQLQuery,(error, results) =>{
+    if (error){
+        console.log(error)
+        res.send(500);
+    }
+    res.send(results);
+  }) 
+
+  connection.end();
+}); */
 
 module.exports = router;
-
