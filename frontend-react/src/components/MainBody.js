9af8ee743091d0823ae49d91b46d9f49956ae8f0
ZenSorcere <zen.sorcere@gmail.com>
2021-02-27 14:01:18 -0800

    Mainbody experiement cleanup

--- frontend-react/src/components/MainBody.js
+++ frontend-react/src/components/MainBody.js
@@ -1,69 +1,15 @@
 import React, { Component } from 'react';
 import Map from "./Map";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+
 
 function MainBody () {
 
         return (
         <div>
-        <Map/>
-        <Router>
-      <div>
-        <nav>
-          <ul className="topnav">
-            <li>
-              <Link to="/">Map</Link>
-            </li>
-            <li>
-              <Link to="/about">Cart</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            {/* adminpage component, not About */}
-            <About />
-          </Route>
-          <Route path="/users">
-            {/* vendorpage component, not Users */}
-            <Users />
-          </Route>
-          <Route path="/">
-            {/* customerpage component, not Home */}
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-      
-      
-    </Router>
-    </div> ) 
+          <Map/>
+          
+        </div> 
+        ) 
     }
-
-
-function Home() {
-  return <h2>Map</h2>;
-}
-
-function About() {
-  return <h2>Cart</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-    
-
 
 export default MainBody;
