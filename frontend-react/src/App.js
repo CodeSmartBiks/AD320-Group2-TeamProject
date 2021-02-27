71daae31b427ef3626ba4d760f79f993fe231d28
ZenSorcere <zen.sorcere@gmail.com>
2021-02-27 10:56:26 -0800

    experiements with feetch setup

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -6,15 +6,50 @@ import MainBody from './components/MainBody';
 import React from "react";
 
 
-function App() {
+//function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      areContactsFetched: false
+    }
+  }
+
+  componentDidMount () {
+    let testData = fetch("http://localhost:3000/users/")
+      .then((results) => {
+        return results.json();
+      }).then ((myJson) => {
+        console.log("FetchREsovled", myJson);
+        this.setState({
+          contacts: myJson,
+          areContactsFetched: true
+        });
+      })
+  }
+          
+  render () {
   return (
     <div className="App">
-     <Header />
-     <MainBody/>
+      {this.state.areContactsFetched || !this.state.contacts ? (
+        <div>loading...</div>
+       ) : (
+          <div>
+            {/*<div>{this.state.contacts}</div>*/}
+            {/*<div>{this.state.contacts[0]}</div>*/}
+            {/*<div>{this.state.contacts[0].Customer_FirstName}</div>*/}
+            <div>{this.state.contacts.Customer_FirstName}</div>
+          </div>
+       )}
+     {/* <Header />
+     <MainBody/> */}
     </div>
   );
+  }
 } 
 
 
 
-export default App;
+//export default App;
