c94ab2c70be5ca36e3110c216658bb33ef19db15
ZenSorcere <zen.sorcere@gmail.com>
2021-03-03 22:44:49 -0800

    add missed save for app js

--- backend-express/app.js
+++ backend-express/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/vendor', vendorRouter);
+//app.use('/vendor', vendorRouter);
 //app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
