f7d376549a3432e549883811de710dd6322af233
ZenSorcere <zen.sorcere@gmail.com>
2021-03-05 16:39:17 -0800

    attempts at pulling order data from db

---
+++ frontend-react/src/components/OrderEntry.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+function OrderEntry ({ orderid, custname, items, total }) {
+   // const testdata = apiResponse.location;
+    return (
+        <div className="order">
+            <img className="userimg" src ={`${window.location.origin}/user-icon.jpg`} />
+            {/* <div className="userimg">User Img</div>{ {float: left;} */}
+           
+            <div className="orderdetails">
+                <h3>{orderid}</h3>
+                <p>{custname}</p>
+                <p>{items}{/*list all items from orderitems for order_id */}</p>
+            </div>
+            <div className="ordertotal"> {/* float:right*/}
+                <h3>Total Due:</h3>
+                <p>{total}{/*total of all items in order*/}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default OrderEntry;
--- frontend-react/src/components/OrdersList.js
+++ frontend-react/src/components/OrdersList.js
@@ -1,15 +1,47 @@
-//import Header from'./Header ';
+import React from 'react';
 import '../index.css';
 import '../Nav.css';
+import OrderEntry from './OrderEntry.js';
 
-function OrdersList  () {
+ class OrdersList extends React.Component {
+    constructor(props) {
+        super(props); 
 
-    return (
-        <div>
-            
-            <h2 className="placeholder">Incomplete Orders</h2>
-        </div>
-    )
-}
+        this.state = {
+            orders: []
+        }
+    }
+
+    componentDidMount () {
+        let test = fetch("http://localhost:3000/vendor/orders/cart/3")
+        .then((results) => {
+            return results.json();
+        }).then((myJson) => {
+            console.log("FetchResolved", myJson);
+            this.setState({
+                orders: myJson
+            });
+        
+        })
+        console.log(this.state.orders); /* array is 0, darnit */
+    }
+
+        render() {
+        return this.state.orders.map((order) => (  
+            <OrderEntry orderid={order.id} custname={order.customer_id} items={order.orderitem_id} total={order.total} />
+            )); 
+     {/*   }         
+            <div>
+                <h2 className="placeholder">Incomplete Orders</h2>
+
+         Ideally this would pull from an array of orders. while the array wasn't empty, would create an OrderEntry for each order in the array.  
+                <OrderEntry orderid="44" custname="Mike" items="hot dog, coke, hunger" total="$9.00" />
+            </div> 
+        );   */}
+        }    
+    
+}  
+
+
 
 export default OrdersList;
