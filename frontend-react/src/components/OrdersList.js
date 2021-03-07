49206360af1b5bdbc4efab78865a990f937fe252
ZenSorcere <zen.sorcere@gmail.com>
2021-03-07 14:11:38 -0800

    correcting save error

--- frontend-react/src/components/OrdersList.js
+++ frontend-react/src/components/OrdersList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Component } from 'react';
 import '../index.css';
 import '../Nav.css';
-import OrderTest from './OrderTest.js';
 import OrderEntry from './OrderEntry.js';
 
  class OrdersList extends React.Component {
@@ -32,26 +31,13 @@ import OrderEntry from './OrderEntry.js';
 
         render() { 
             const OrderList = this.state.orders.map(order => {
-                /*return <OrderTest order={order} id={order.id} />; */
-
                 return <OrderEntry key={order.Order_Id} order={order}  />;
             });
-        return (
-            <div>
-                {OrderList}
-            </div>
+            return (
+                <div>
+                    {OrderList}
+                </div>
             )
-        
-        
-             
-     {/*   }         
-            <div>
-                <h2 className="placeholder">Incomplete Orders</h2>
-
-         Ideally this would pull from an array of orders. while the array wasn't empty, would create an OrderEntry for each order in the array.  
-                <OrderEntry orderid="44" custname="Mike" items="hot dog, coke, hunger" total="$9.00" />
-            </div> 
-        );   */}
         }    
     
 }  
