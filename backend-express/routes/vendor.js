0c0733c352d7b9c7354a564e5288275c92c18693
Biks <bikasjthapa@gmail.com>
2021-03-07 19:00:34 -0800

    This branch is pushed because my put method for longitude is hitting error

--- backend-express/routes/vendor.js
+++ backend-express/routes/vendor.js
@@ -1,12 +1,13 @@
+const e = require('express');
 var express = require('express');
 let mysql = require('mysql2');
 let dbCreds = require("../../../dbCreds.json");
 var router = express.Router();
 
 /* VENDOR - default path response */
-router.get('/', function(req, res, next) {
-    res.send('please include cart number.');
-  });
+router.get('/', function (req, res, next) {
+  res.send('please include cart number.');
+});
 
 
 
@@ -47,57 +48,76 @@ router.get('/orders/cart/:id', function (req, res, next) {
 });
 
 /* VENDOR - Retrieve all details for a specific order */
-router.get('/orders/order/:id', function(req, res, next) {
-  
-    let connection = mysql.createConnection(dbCreds);
-    connection.connect();
-  
-    connection.query(`SELECT DISTINCT orders.Order_Id,SUM(Menu_Price * Quantity) AS "Total",Customer_FirstName,Order_Date,Order_Status, Cart_Id, group_concat(concat(Menu_Name, '(', Quantity, ') - ', Menu_Price, ' ea')SEPARATOR', ') as "items" FROM orders 
+router.get('/orders/order/:id', function (req, res, next) {
+
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+
+  connection.query(`SELECT DISTINCT orders.Order_Id,SUM(Menu_Price * Quantity) AS "Total",Customer_FirstName,Order_Date,Order_Status, Cart_Id, group_concat(concat(Menu_Name, '(', Quantity, ') - ', Menu_Price, ' ea')SEPARATOR', ') as "items" FROM orders 
     INNER JOIN ordersdetails ON orders.Order_Id = ordersdetails.Order_Id
     INNER JOIN ordersitems ON ordersdetails.OrderItem_Id = ordersitems.OrderItem_Id
     INNER JOIN menu ON menu.Menu_Id = ordersitems.Menu_Id 
     INNER JOIN customer ON customer.Customer_Id = orders.Customer_Id
-    WHERE orders.Order_Id = ?;`,[req.params.id],  (error, results) =>{
-  
-      if (results.length === 0) {
-        res.status(404).send("Order Not Found");
-      }
-      else if (error) {
-        res.sendStatus(500);
-      } else {
-        res.status(200).send(results);
-      }
-    })
-    connection.end();
-  });
+    WHERE orders.Order_Id = ?;`, [req.params.id], (error, results) => {
+
+    if (results.length === 0) {
+      res.status(404).send("Order Not Found");
+    }
+    else if (error) {
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(results);
+    }
+  })
+  connection.end();
+});
 
 /* VENDOR/ADMIN - GET all Menu items for a specific cart */
 /* this is also on the admin.js */
 router.get('/menus/cart/:id', function (req, res, next) {
-    let connection = mysql.createConnection(dbCreds);
-    connection.connect();
-  
-    connection.query(`SELECT Cart_Id, cartmenus.Menu_Id, Menu_Name, Menu_Description, Menu_Price, Available
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+
+  connection.query(`SELECT Cart_Id, cartmenus.Menu_Id, Menu_Name, Menu_Description, Menu_Price, Available
     FROM cartmenus
     INNER JOIN menu ON cartmenus.Menu_Id = menu.Menu_Id
     WHERE Cart_Id = ?;`, [req.params.id], (error, results) => {
-      /* if req.params.id doesn't match a Cart_Id, return 404 http status and a "cart not found" msg */
-      if (results.length === 0) {
-  
-        res.status(404).send("Cart Not Found");
+    /* if req.params.id doesn't match a Cart_Id, return 404 http status and a "cart not found" msg */
+    if (results.length === 0) {
 
+      res.status(404).send("Cart Not Found");
+
+    }
+    else if (error) {
+      res.sendStatus(500);
+    } else {
+
+      res.status(200).send(results);
+    }
+  })
+
+  connection.end();
+
+});
+
+//Issue 33 PUT endpoint to update cart availability
+router.put('/carts/:id',(req, res) =>{
+  let connection = mysql.createConnection(dbCreds);
+  connection.connect();
+  let longitude= '${req.body.Longitude}';
+  let id='${req.params.id}';
+
+
+   let mySQLQuery = `UPDATE Carts SET Longitude = ${longitude} WHERE Cart_Id = ${id}`;
+   Connection.Query(mySQLQuery,(error, results) => {
+     
+      if(error) {
+         res.send(500);
       }
-      else if (error) {
-        res.sendStatus(500);
-      } else {
-  
-        res.status(200).send(results);
-      }
-    })
+      res.status(201).send(results);
+      })
 
-    connection.end();
-  
-  });
-
+   Connection.end();
+});
 
 module.exports = router;
