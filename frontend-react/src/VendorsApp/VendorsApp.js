bf65de0b8d669479ae1f97847c920ead433e47e3
Crystal Broderick <broderick.crystal@gmail.com>
2021-03-08 21:25:32 -0800

    Cleaned up VendorsApp comments

--- frontend-react/src/VendorsApp/VendorsApp.js
+++ frontend-react/src/VendorsApp/VendorsApp.js
@@ -1,32 +1,22 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import VendorSettings from './VendorSettings'
-
+import React from 'react';
 import '../index.css';
 import '../Nav.css';
 import Header from'../components/Header';
 import VendorMenu from './VendorMenu';
-import MenuList from './MenuList';
-import React, { Component } from 'react';
-import MenuDetail from './MenuDetail';
-import MenuPanel from './MenuPanel';
-import './Toggle.css'
 
-//function VendorsApp() {
+
 
 class VendorsApp extends React.Component {
-    constructor(props){
-    super(props);
-    }
-
-
     render() {
     return (
         <div>
             <Header />
-            <MenuPanel/>                    
+            <h2 className="placeholder">VendorsApp Container</h2>
+            <VendorMenu />
+
+            
         </div>
-    );
+    )
 }
 }
 export default VendorsApp;
