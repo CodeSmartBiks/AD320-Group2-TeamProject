016a9f376b31fcf5eaa64a5a1e64fbb2dbc1c773
Crystal Broderick <broderick.crystal@gmail.com>
2021-03-18 17:34:53 -0700

    show remove buttons

--- frontend-react/src/CustomersApp/MenuItem.js
+++ frontend-react/src/CustomersApp/MenuItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const MenuItem = ({menuItem, addToBasket, quantity}) => {
+const MenuItem = ({menuItem, addToBasket, removeFromBasket, Quantity}) => {
     return(
   
       <div className="row">
         <div className="column left">  
           <h2>{menuItem.Menu_Name} ${menuItem.Menu_Price} </h2>
           {menuItem.Menu_Description}</div>
-          <div className="column right">
+          <div className="column right"><button onClick={() => removeFromBasket(menuItem.Menu_Name)}>-</button>
           <button onClick={() => addToBasket(menuItem)}>+</button></div>
           </div>
           
